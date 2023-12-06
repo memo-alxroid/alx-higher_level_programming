ec51b1a87379e4dff89160a517bc91586973b99e
Mina Sami <memo.samy0050@gmail.com>
2023-12-05 16:04:37 -0800

    prints numbers from 0 to 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number < 10:
+        print("0{}".format(number), end=", ")
+    else:
+        if number != 99:
+            print("{}".format(number), end=", ")
+        else:
+            print("{}".format(number))
