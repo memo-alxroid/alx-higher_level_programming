500dc3e005196356ec829baa0a150d7a2236ee52
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-06 02:06:33 +0200

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number < 10:
-        print("0{}".format(number), end=", ")
+    if number == 99:
+        print("{}".format(number))
     else:
-        if number != 99:
-            print("{}".format(number), end=", ")
-        else:
-            print("{}".format(number))
+        print("{:02}".format(number), end=", ")
