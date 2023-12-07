33ce7fb06cf803b83c391b226475180027aba6d8
Mina Sami <memo.samy0050@gmail.com>
2023-12-06 18:58:27 -0800

    the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print arguments and their numbers"""
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(1))
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv)):
+            if i == 0:
+                continue
+            print("{}: {}".format(i, sys.argv[i]))
