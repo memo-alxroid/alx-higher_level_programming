0ee17024af8be50eda76bc34b6ca0c946dddced7
Mina Sami <memo.samy0050@gmail.com>
2023-12-08 15:41:07 -0800

    returns a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return None
+    max_key = None
     max_value = 0
     for key, value in a_dictionary.items():
         if value >= max_value:
