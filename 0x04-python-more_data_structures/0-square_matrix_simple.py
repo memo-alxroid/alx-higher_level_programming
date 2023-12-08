5a60361929f8fc44aa78331eb607c1d020e73143
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-08 22:43:46 +0200

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 
 def square_matrix_simple(matrix=[]):
     newMatrix = matrix.copy()
-    newMatrix = [[lists[i] * lists[i] for i in range(len(matrix))]
+    newMatrix = [[lists[i] * lists[i] for i in range(len(lists))]
                  for lists in matrix]
     return newMatrix
