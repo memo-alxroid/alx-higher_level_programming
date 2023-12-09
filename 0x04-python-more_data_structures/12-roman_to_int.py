0f3a8a1b1144d98bdcfc4650ca57e031f3844311
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-09 03:34:36 +0200

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,21 +5,15 @@ def roman_to_int(roman_string):
         return 0
     result = 0
     roman_dict = {"M": 1000, "C": 100, "D": 500, "L": 50,
-                  "V": 5, "X": 10,"I": 1}
-    roman_dict_subtractive = {"IV": 4, "IX": 9, "XL":40,
+                  "V": 5, "X": 10, "I": 1}
+    roman_dict_subtractive = {"IV": 4, "IX": 9, "XL": 40,
                               "XC": 90, "CD": 400, "CM": 900}
-    
-    for i in range(0, len(roman_string),2):
-        if i < len(roman_string) - 1:
-            two_chars = roman_string[i] + roman_string[i + 1]
-            if roman_dict_subtractive.__contains__(two_chars):
-                result += roman_dict_subtractive[two_chars]
-            elif roman_dict.__contains__(two_chars[0]):
-                result += roman_dict[two_chars[0]]
-            elif roman_dict.__contains__(two_chars[1]):
-                result += roman_dict[two_chars[1]]
-            else:
-                result += 0
+    i = 0
+    while i < len(roman_string):
+        if i + 1 < len(roman_string) and roman_string[i:i + 2] in roman_dict_subtractive:
+            result += roman_dict_subtractive[roman_string[i:i + 2]]
+            i += 2  # Increment by 2 since we've processed two characters
         else:
             result += roman_dict[roman_string[i]]
+            i += 1  # Increment by 1 for single characters
     return result
