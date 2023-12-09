07c587ff1ce70a8c07a0b10dbdc186b8a9c55798
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-09 02:56:34 +0200

    solve test 7

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,19 +4,22 @@ def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
     result = 0
-    roman_dict = {"M": 1000, "C": 100, "D": 500, "L": 50, "V": 5,
-                  "X": 10, "I": 1}
-    roman_dict_sub = {"IV": 4, "IX": 9, "XL": 40, "XC": 90,
-                      "CD": 400, "CM": 900}
-
-    for i in range(0, len(roman_string), 2):
+    roman_dict = {"M": 1000, "C": 100, "D": 500, "L": 50,
+                  "V": 5, "X": 10,"I": 1}
+    roman_dict_subtractive = {"IV": 4, "IX": 9, "XL":40,
+                              "XC": 90, "CD": 400, "CM": 900}
+    
+    for i in range(0, len(roman_string),2):
         if i < len(roman_string) - 1:
             two_chars = roman_string[i] + roman_string[i + 1]
-            if roman_dict_sub.__contains__(two_chars):
-                result += roman_dict_sub[two_chars]
-            else:
+            if roman_dict_subtractive.__contains__(two_chars):
+                result += roman_dict_subtractive[two_chars]
+            elif roman_dict.__contains__(two_chars[0]):
                 result += roman_dict[two_chars[0]]
+            elif roman_dict.__contains__(two_chars[1]):
                 result += roman_dict[two_chars[1]]
+            else:
+                result += 0
         else:
             result += roman_dict[roman_string[i]]
     return result
