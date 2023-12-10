00f4baa8c1638403c770aeb21f63f02673764d70
Mina Sami <memo.samy0050@gmail.com>
2023-12-09 17:26:06 -0800

    prints all integers of a list

--- 0x03-python-data_structures/0-square_matrix_simple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-    newMatrix = matrix
-    newMatrix = [[lists[i] * lists[i] for i in range(len(matrix))]
-                 for lists in matrix]
-    return newMatrix
