ae11bbc7d75b43be9c449a6957587a462d46b231
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-10 04:23:34 +0200

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    return len(sentence) if sentence else 0,sentence[0] if sentence else None
+    return len(sentence) if sentence else 0, sentence[0] if sentence else None
