d311ee6f1eeef89780ec1bf4588c8d36e4d102ec
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-10 04:07:37 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
             for i in row:
-                print("{}".format(i), end=" ")
+                print("{:d}".format(i), end=" ")
             print()
