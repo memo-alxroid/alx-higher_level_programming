8669a3919f9891a7f050f779c3be015ff172f8ed
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-10 04:06:53 +0200

    Create 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for i in row:
+                print("{}".format(i), end=" ")
+            print()
