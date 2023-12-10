efceb1e92a651fe5404f4cc803581070f2b75dda
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-10 03:54:01 +0200

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx > len(my_list) - 1 or idx < 0:
         return new_list
