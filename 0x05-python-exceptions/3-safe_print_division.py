ee2d73cf2273356de30e87a613f805f64673d7d2
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-20 00:53:33 +0200

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         quotient = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         quotient = None
     finally:
         print("Inside result: {}".format(quotient))
