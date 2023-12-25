cab0d86ec5a2f0d37980b573d90dc96629f1a976
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-25 03:37:15 +0200

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initializes the data."""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
