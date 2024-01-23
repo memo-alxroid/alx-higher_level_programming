d8c7ea1994b78ececda3d8526e515108be5a3f4a
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-01-23 05:29:27 +0200

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,21 +7,25 @@ def matrix_divided(matrix, div):
     Returns: new_matrix (list of lists): new matrix with quotients.
     """
     new_matrix = []
+    error_msgs = ["matrix must be a matrix (list of lists) of integers/floats",
+                  "Each row of the matrix must have the same size",
+                  "div must be a number",
+                  "division by zero"]
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(error_msgs[0])
     list_length = len(matrix)
     for i in range(list_length):
         quotients_list = []
         for j in range(len(matrix[i])):
             if i != list_length - 1:
                 if len(matrix[i]) != len(matrix[i + 1]):
-                    raise TypeError("Each row of the matrix must have the same size")
+                    raise TypeError(error_msgs[1])
             if type(div) is not int and type(div) is not float:
-                raise TypeError("div must be a number")
+                raise TypeError(error_msgs[2])
             if div == 0:
-                raise ZeroDivisionError("division by zero")
+                raise ZeroDivisionError(error_msgs[3])
             if type(matrix[i][j]) is not int and type(matrix[i][j]) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(error_msgs[0])
             quotients_list.append(round(matrix[i][j] / div, 2))
         new_matrix.append(quotients_list)
     return new_matrix
