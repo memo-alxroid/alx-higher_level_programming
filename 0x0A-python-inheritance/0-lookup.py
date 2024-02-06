635d369a4449bb9969e44302b20d9d0991ca778e
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-02-06 23:55:57 +0200

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module to return the list of available attributes and methods of an object"""
+"""Module to return the list of 
+available attributes and methods of an object"""
 
 
 def lookup(obj):
