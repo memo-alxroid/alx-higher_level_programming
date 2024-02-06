e249740d5f8042dc4a709dba8a4bb49b331b1cf1
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-02-07 01:23:45 +0200

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that have a function that returns True 
+"""Module that have a function that returns True
 if the object is an instance of, or if the object
 is an instance of a class that inherited from,
 the specified class; otherwise False"""
