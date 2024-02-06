f0790d558dd55048274e4030899f64c9f30f48b7
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-02-07 01:34:10 +0200

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
      is an instance of a class that inherited,
      (directly or indirectly) from the specified
      class"""
-    return issubclass(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
