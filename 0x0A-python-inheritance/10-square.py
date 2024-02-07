7826095ca848bd1e617bda1c0b6b3040a5428c7c
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-02-07 02:20:46 +0200

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,10 +12,6 @@ class Square(Rectangle):
         self.__size = self.integer_validator("size", size)
         super().__init__(self.__size, self.__size)
 
-    def __str__(self):
-        """Method that returns the string representation of the instance"""
-        return "[Square] " + str(self.__size) + "/" + str(self.__size)
-
     def area(self):
         """Method that returns the area of the square"""
         return self.__size ** 2
