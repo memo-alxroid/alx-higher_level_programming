baf97aaac6db02966cab0cc50f4fc898cc3bcdb2
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-02-07 02:17:52 +0200

    Create 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+class Square(Rectangle):
+    """Square Class that inherits from Rectangle"""
+    def __init__(self, size):
+        """Method that initializes the instance"""
+        self.__size = self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+        
+    def __str__(self):
+        """Method that returns the string representation of the instance"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
+    
+    def area(self):
+        """Method that returns the area of the square"""
+        return self.__size ** 2
