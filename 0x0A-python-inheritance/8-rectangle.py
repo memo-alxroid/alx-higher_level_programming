13e1f088a556840e3bb4e0ea5ca136523ed2c2d4
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-02-07 02:01:56 +0200

    Create 8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module of BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class that has a method area()
+     that raises an Exception"""
+    def area(self):
+        """Method that raises an Exception"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """Method that validates value"""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+        return value
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """Method that initializes the instance"""
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
