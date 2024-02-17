c7483e0199af0d2ac6525943e6e287c72dce8473
Mina Sami <memo.samy0050@gmail.com>
2024-02-17 09:31:08 -0800

    added square(cls, size=0) class method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -58,7 +58,8 @@ class Rectangle:
         """The __str__ method of the class."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return ((Rectangle.print_symbol * self.__width + "\n") * self.__height)[:-1]
+        return ((Rectangle.print_symbol * self.__width + "\n")
+                * self.__height)[:-1]
 
     def __repr__(self):
         """The __repr__ method of the class."""
