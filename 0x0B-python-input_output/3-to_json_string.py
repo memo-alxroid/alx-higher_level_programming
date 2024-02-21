63d32a917f416e8642efab24c1e10fc9224a4a3f
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-02-21 07:49:24 +0200

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the JSON representation of an object (string) """
+""" Module that contains a function that returns
+the JSON representation of an object (string) """
 import json
 
 
