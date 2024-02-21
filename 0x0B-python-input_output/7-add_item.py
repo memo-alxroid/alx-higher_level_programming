936ea77a54ae556b47555a5072303481790bcbba
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2024-02-21 08:24:05 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,10 +7,7 @@ import sys
 if __name__ == "__main__":
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    main()
-
-def main():
-    """ Main function of the module """
+    
     filename = "add_item.json"
     try:
         my_list = load_from_json_file(filename)
